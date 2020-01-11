cb918ee84eb6827242b4b24709da687ef958ac31
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-11 00:16:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -101,7 +101,6 @@ def movies_with_directors_set(source)
     }
     ]
 end
-end
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
